0e3b6d77c56d1d830f3afff297dbd1a0d610cfd7
yamgemy <yamgemy@gmail.com>
2024-05-30 18:50:12 +0700

    setup vector icons on ios

--- src/screens/search-banana-owners-screen/component.tsx
+++ src/screens/search-banana-owners-screen/component.tsx
@@ -3,13 +3,12 @@ import { ScalingTouchable } from "@src/components";
 import { useThemeChoice } from "@src/hooks/use-theme-choice";
 import React, { FC, useCallback } from "react";
 import { View } from "react-native";
-import { Text, useTheme } from "react-native-paper";
+import { Searchbar, Text, useTheme } from "react-native-paper";
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useDispatch } from "react-redux";
 import { getThemedStyles } from "./styles";
+MaterialCommunityIcon.loadFont();
 
-/*
-
-*/
 export const SearchBananaOwnersScreen:FC<any> = () => {
   const dispatch = useDispatch();
   const {getChosenAppTheme} = useThemeChoice();
@@ -22,11 +21,18 @@ export const SearchBananaOwnersScreen:FC<any> = () => {
     dispatch(setAppThemePO(nextThem));
   },[dispatch,getChosenAppTheme]);
 
+  const [searchQuery, setSearchQuery] = React.useState('');
+
   return ( 
     <View style={styles.root}>
       <ScalingTouchable onPress={toggleTheme}>
         <Text theme={theme}>Toggle Theme</Text>
-      </ScalingTouchable> 
+      </ScalingTouchable>
+      <Searchbar
+          placeholder="Search"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+    />
     </View>
   );
 };
