db16a7ef45ad1fd06e644650bd8d0af7de9270db
yamgemy <yamgemy@gmail.com>
2024-06-01 01:16:14 +0700

    add rank text at LeaderboardAutosuggestItem

--- src/components/leaderboard-autosuggest-item/component.tsx
+++ src/components/leaderboard-autosuggest-item/component.tsx
@@ -1,15 +1,16 @@
 import HighlightText from '@sanar/react-native-highlight-text';
 import { colors } from '@src/constants';
-import { LeaderboardItemProps } from '@src/mockdata/types';
+import { LeaderboardItemWithExtraProps } from '@src/mockdata/types';
 import React, { FC, useCallback } from 'react';
-import { Surface, useTheme } from 'react-native-paper';
+import { View } from 'react-native';
+import { Surface, Text, useTheme } from 'react-native-paper';
 import { useDebouncedCallback } from 'use-debounce';
 import { ScalingTouchable } from '../scaling-touchable';
 import { getThemedStyles } from './styles';
 
 export type LeaderboardAutosuggestItemProps = {
   onSelect: (selection:string)=>void
-  entity: LeaderboardItemProps
+  entity: LeaderboardItemWithExtraProps
   searchQuery: string
 }
 export const LeaderboardAutosuggestItem:FC<LeaderboardAutosuggestItemProps> = ({
@@ -27,15 +28,18 @@ export const LeaderboardAutosuggestItem:FC<LeaderboardAutosuggestItemProps> = ({
   return (
     <ScalingTouchable onPress={handleSuggestionItemPress}>
       <Surface style={styles.suggestionRow} mode='elevated' elevation={1}>
-        <HighlightText
-            highlightStyle={styles.highlight}
+        <View style={styles.suggestionTextWrap}>
+          <HighlightText
+              highlightStyle={styles.highlight}
             // @ts-ignore
-            searchWords={[searchQuery]}
-            style={styles.suggestionText}
-            selectionColor={colors.red_500}
-            caseSensitive={false}
-            textToHighlight={entity.name}
+              searchWords={[searchQuery]}
+              style={styles.suggestionText}
+              selectionColor={colors.red_500}
+              caseSensitive={false}
+              textToHighlight={entity.name}
         />
+          <Text>Rank: {entity.rank}</Text>
+        </View>
       </Surface>
     </ScalingTouchable>
   );
